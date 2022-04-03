835c9b825edcd72617a0178690054a39ef9edd8d
Olivier Cagnat <oliviercagnat@gmail.com>
2022-04-03 16:28:30 +0900

    controller and routes done

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,48 @@
+class RestaurantsController < ApplicationController
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new # needed to instantiate the form_for
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+
+    # no need for app/views/restaurants/create.html.erb
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+
+    # no need for app/views/restaurants/update.html.erb
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+
+    # no need for app/views/restaurants/destroy.html.erb
+    redirect_to restaurants_path
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :rating)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,19 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # See all restaurants
+  get "/restaurants", to: "restaurants#index"
+  # Create a restaurant
+  get "restaurants/new", to: "restaurants#new" # The `new` route needs to be *before* `show` route.
+  post "restaurants", to: "restaurants#create"
+  # See details about one restaurant
+  get "restaurants/:id", to: "restaurants#show", as: :restaurant
+  # Update a restaurant
+  get "restaurants/:id/edit", to: "restaurants#edit"
+  patch "restaurants/:id", to: "restaurants#update"
+  # Destroy a restaurant
+  delete "restaurants/:id", to: "restaurants#destroy"
+
+
 end
